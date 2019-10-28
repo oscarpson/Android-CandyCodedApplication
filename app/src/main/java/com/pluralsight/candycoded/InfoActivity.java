724f2720ba8ea5b307267461bfe8c6a5696f90a6
OscarMuigai <muigai.oscar@ekenya.co.ke>
2019-10-28 17:37:22 +0300

    added dial intent

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -32,6 +32,12 @@ public class InfoActivity extends AppCompatActivity {
             startActivity(mapIntent);
         }
     }
+    public void createPhoneIntent(View view){
+    Intent callIntent = new Intent(Intent.ACTION_DIAL);
+    Uri uri= Uri.parse("tel:0123456789");
+    callIntent.setData(uri);
+    startActivity(callIntent);
+    }
 
     // ***
     // TODO - Task 2 - Launch the Google Maps Activity
